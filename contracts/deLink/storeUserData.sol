a09007c6a2d90fbe7adf6492fd0eacb7ebf59fb9
dxfareed <einstein1101@proton.me>
2024-10-06 12:29:44 +0100

    onlyOneStore

--- contracts/deLink/storeUserData.sol
+++ contracts/deLink/storeUserData.sol
@@ -1,14 +1,23 @@
-//SPDX-License-Identifer: MIT
-
+//SPDX-License-Identifer: UNLICENSED
 pragma solidity ^0.8.17;
 
 contract storeUserData{
     struct eachList{
+        string name;
         string topic;
-        string link;   
+        string link;
+        string uid; 
     }
     mapping( address => eachList[]) StoreList;
-    function addList(eachList[] memory) external{
-        StoreList[msg.sender]=eachList[];
+    function addList(eachList[] memory _userlist) external{
+        for(uint i; i<_userlist.length; i++){
+            StoreList[msg.sender].push(_userlist[i]);
+        }
+        //StoreList[msg.sender].push(_userlist);
+    }
+    function checkUser() external view returns(string memory  _message){
+        if(StoreList[msg.sender].length!=0){
+            return StoreList[msg.sender][0].uid;
+        }
     }
 }
