d8cc31d446153dad4d08cfd2e2a364a12dbc1f15
dxfareed <einstein1101@proton.me>
2025-01-06 01:31:53 +0100

    up

--- contracts/shareAdminReward.sol
+++ contracts/shareAdminReward.sol
@@ -14,38 +14,20 @@ contract TrivBank{
     //address private immutable MAINNET_USDC = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
     address private immutable MOCK_USDC = 0x1241676d45b1Cb5B573b6258C4A838e149A1D191;
 
-    address[2] admins = [0x52c043C7120d7DA35fFdDF6C5c2359d503ceE5F8, 0x70ca4a44A227645BB4815AE4d68098eA68aB926F] ;
-    
+    address private immutable admin1 = 0x70ca4a44A227645BB4815AE4d68098eA68aB926F;
+    address private immutable admin2 = 0x52c043C7120d7DA35fFdDF6C5c2359d503ceE5F8;
+ 
     function Withdrawal4Work(uint _amount,address _userAddr2send) external onlyVerfAdd {
         IERC20(MOCK_USDC).transfer(_userAddr2send, _amount);
     }
 
-    function ContractTriviaBalance() public view returns (uint){
+    function ContractTriviaBalance() public view onlyVerfAdd returns (uint){
         return IERC20(MOCK_USDC).balanceOf(address(this));
     }
 
-    /* function PayAdmins(address[7] memory _admins, uint amount) external onlyVerfAdd {
-        for (uint i = 1; i < _admins.length; ++i) {
-            IERC20(MOCK_USDC).transfer(_admins[i], amount); 
-        }        
-    } */
-
     modifier onlyVerfAdd(){
-        if( (msg.sender == address(admins[0])) || (msg.sender == address(admins[1])) ){
-            revert("Caller is not Admin");
-        }
+        require( (msg.sender == admin1) || (msg.sender == admin2),"Caller not valid");
         _;
     }
-
-    /* 
-    
-    function EmergencyWithdrawalAdmin() external onlyAddress{
-        uint contractBalance = IERC20(MAINNET_USDC).balanceOf(address(this));
-        IERC20(MAINNET_USDC).transfer(ADMIN, contractBalance);
-        
-   
-
-    */
-
 
 }
