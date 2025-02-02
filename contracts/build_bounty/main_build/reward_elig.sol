28a166638a977cac9b6a6cee575090867c082962
dxfareed <einstein1101@proton.me>
2025-02-03 00:29:34 +0100

    refact gm

--- contracts/build_bounty/main_build/reward_elig.sol
+++ contracts/build_bounty/main_build/reward_elig.sol
@@ -12,29 +12,36 @@ interface IERC20 {
 
 //100_000_000_000_000_000_000_000_000
 
-
 //10000000000000
 
 //[0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db]
 
-
 contract RewardElig {
-    address private immutable admin;
-    uint256 private immutable PERIOD;
+
+    uint256 private constant POOL_AMOUNT = 10**7;
+    uint256 private constant magic_num = 10**18;
+    uint256 private constant DURATION_PERIOD = 5 minutes;
+
+    
     address private immutable MOCK_BUILD;
-    uint256 private immutable POOL_AMOUNT; 
+    mapping(address => uint256) private EligibleUser;
+
+    address[] private EligibleUserClaimWeek;
+    address[] private EligibleUserArr;
+    address private admin;
+    uint256 public CurrentWeekStart;
+
+    
+    
+
     constructor(address _build_ca) {
-        admin = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+        admin = msg.sender;
         MOCK_BUILD = _build_ca;
-        POOL_AMOUNT = 10**7; 
-        PERIOD = 10 weeks;
+        CurrentWeekStart = block.timestamp - (block.timestamp % DURATION_PERIOD);
     }
 
-    mapping(address => uint256) EligibleUser;
-    uint256 public TotalClaimPerWeek;
-    address[] EligibleUserArr;
-
-    event ClaimRewardUser(address _user, uint _amount);
+    event ClaimRewardUser(address _user, uint256 _amount);
+    event TotalUserClaim(uint256 _total_num_user);
 
     error DuplicateUserAdded(string);
     error IneligibleUser(string);
@@ -63,7 +70,6 @@ contract RewardElig {
     }
 
     function RemoveEligibleUser(address _user) external onlyAdmin {
-        
         uint256 _len = EligibleUserArr.length;
 
         delete EligibleUser[_user];
@@ -91,33 +97,56 @@ contract RewardElig {
 
         if (EligibleUser[msg.sender] == 1) {
             // remove 1 after check :)
-            //user first entry
-            _lastTime = (EligibleUser[msg.sender] + 5 minutes) - 1;
-        }
-        else{
-            _lastTime = (EligibleUser[msg.sender] +  5 minutes);
+            _lastTime = (EligibleUser[msg.sender] +  5 minutes ) - 1;
+        } else if (EligibleUser[msg.sender] > 1) {
+            _lastTime = (EligibleUser[msg.sender] +  5 minutes );
         }
 
-        require(block.timestamp >= _lastTime, "Claim after 5 minutes");
+        require(block.timestamp >= _lastTime, "Once per week");
         require(
             IERC20(MOCK_BUILD).balanceOf(address(this)) > 0,
             "Empty vault!"
         );
+
+      
         //end check, get reward
         //pool of 10m, share reward based on total num of users in the pool.
 
+    
+       
+
         uint256 each_portion = POOL_AMOUNT / _totalUser;
 
-        IERC20(MOCK_BUILD).transfer(msg.sender, each_portion);
-
+        IERC20(MOCK_BUILD).transfer(msg.sender, each_portion * magic_num);
         EligibleUser[msg.sender] = block.timestamp;
+
+
+        for( uint16 i; i <EligibleUserClaimWeek.length; i++){
+            //little reset logic
+
+            if(EligibleUserClaimWeek[i] == msg.sender){
+                delete EligibleUserClaimWeek;
+                EligibleUserClaimWeek.push(msg.sender);
+                emit TotalUserClaim(EligibleUserClaimWeek.length);
+                emit ClaimRewardUser(msg.sender, each_portion);
+                return 0;
+            }
+        }
+
+        emit TotalUserClaim(EligibleUserClaimWeek.length);
+        emit ClaimRewardUser(msg.sender, each_portion);
+        EligibleUserClaimWeek.push(msg.sender);
         return 0;
     }
 
-    uint timeAweek = block.timestamp;
-
-
     function ClaimReward() external returns (uint256) {
+
+        if (block.timestamp >= CurrentWeekStart + DURATION_PERIOD ) {
+            ResetTotalClaim();
+            CurrentWeekStart = block.timestamp - (block.timestamp % DURATION_PERIOD );
+        }
+
+
         uint256 _lastTime;
         uint256 _totalUser = EligibleUserArr.length;
         if (EligibleUser[msg.sender] == 0) {
@@ -126,10 +155,10 @@ contract RewardElig {
 
         if (EligibleUser[msg.sender] == 1) {
             // remove 1 after check :)
-            _lastTime = (EligibleUser[msg.sender] + 1 weeks) - 1;
+            _lastTime = (EligibleUser[msg.sender] +  DURATION_PERIOD ) - 1;
+        } else if (EligibleUser[msg.sender] > 1) {
+            _lastTime = (EligibleUser[msg.sender] +  DURATION_PERIOD );
         }
-
-        _lastTime = (EligibleUser[msg.sender] + 1 weeks);
 
         require(block.timestamp >= _lastTime, "Once per week");
         require(
@@ -137,46 +166,55 @@ contract RewardElig {
             "Empty vault!"
         );
 
-        //end check, get reward
-        //pool of 10m, share reward based on total num of users in the pool.
-
+      
+        bool alreadyClaimed = false;
+        for (uint16 i; i < EligibleUserClaimWeek.length; i++) {
+            if (EligibleUserClaimWeek[i] == msg.sender) {
+                alreadyClaimed = true;
+                break;
+            }
+        }
+        if (!alreadyClaimed) {
+            EligibleUserClaimWeek.push(msg.sender);
+        }
+       
         uint256 each_portion = POOL_AMOUNT / _totalUser;
 
-        IERC20(MOCK_BUILD).transfer(msg.sender, each_portion);
-
+        IERC20(MOCK_BUILD).transfer(msg.sender, each_portion * magic_num);
         EligibleUser[msg.sender] = block.timestamp;
+
         emit ClaimRewardUser(msg.sender, each_portion);
-
-        //eachclaim
-        if( block.timestamp < (timeAweek + 1 minutes)){
-            TotalClaimPerWeek++;
-        }
-        else{
-            TotalClaimPerWeek = 0;
-            timeAweek = block.timestamp;
-        }
-                
-
         return 0;
     }
 
-    /*  uint8[] test = [1, 2, 3, 4, 5];
+    
+    function ResetTotalClaim() internal {
+        delete EligibleUserClaimWeek;
+    }
 
-    function arraytest(uint8 _index) external returns(uint8[] memory){
-        
-        for(uint8 i = _index; i < test.length - 1; i++){
-            test[i] = test[i + 1]; 
-        }
-        test.pop();
-        return test;
-    } */
+    function TotalUserAWeek() external view returns(uint){
+        return EligibleUserClaimWeek.length;
+    }
+
+    function TotalAddressAWeek() external view returns(address[] memory){
+        return EligibleUserClaimWeek;
+    
+    }
+
+    function ChangeAdmin(address _newAdmin) external onlyAdmin{
+        admin = _newAdmin;
+    }
+
+    function CurrentAdmin() external view returns(address){
+        return admin;
+    }
 
     function lis() external view returns (address[] memory) {
         return EligibleUserArr;
     }
 
     function ReadOnlyBalance4Dev() external view returns (uint256) {
-        return IERC20(MOCK_BUILD).balanceOf(address(this)) / 10**18;
+        return IERC20(MOCK_BUILD).balanceOf(address(this)) / magic_num;
     }
 
     function returndf(address _user) external view returns (uint256) {
